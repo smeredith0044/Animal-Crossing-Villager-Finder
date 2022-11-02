ccc504bc0a7d0c28cc7b2758f09758e88ba2f899
smeredith0044 <sydney.meredith@ymail.com>
2022-11-02 13:50:14 -0400

    sey up base console app and called an api to retrieve all results

---
+++ VillagerSearch/JsonFileUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json.Linq;
+
+namespace VillagerSearch
+{
+    // Native/JsonFileUtils.cs
+    public static class JsonFileUtils
+    {
+        public static void SimpleWrite(string json, string fileName)
+        {
+            Console.WriteLine(File.Exists(fileName));
+            File.WriteAllText(fileName, json);
+            var data = (JObject)JsonConvert.DeserializeObject(json);
+            var firstVillagerRecord = data.SelectToken(
+               "ant00").Value<string>();
+            Console.WriteLine(firstVillagerRecord);
+
+        }
+    }
+}
+
---
+++ VillagerSearch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using VillagerSearch;
+namespace HttpClientStatus;
+
+class Program
+{
+
+    // Main Method
+    static async Task Main(String[] args)
+    {
+
+        // Type your username and press enter
+       // Console.WriteLine("Enter villager name:");
+
+        // Create a string variable and get user input from the keyboard and store it in the variable
+       // string villagerName = Console.ReadLine();
+
+        // Print the value of the variable (userName), which will display the input value
+       // Console.WriteLine("villager is: " + villagerName);
+
+        using var client = new HttpClient();
+
+        var result = await client.GetAsync("https://acnhapi.com/v1/villagers/");
+        Console.WriteLine(result.StatusCode);
+        string responseBody = await result.Content.ReadAsStringAsync();
+        JsonFileUtils.SimpleWrite(responseBody, "/Users/sydney/Sydney_final_project/VillagerSearch/VillagerSearch/VillagerData.json");
+    }
+}
+
