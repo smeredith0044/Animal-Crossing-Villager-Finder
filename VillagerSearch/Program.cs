2b38c7bf1eadc7178892fcc6e040479f2506b82a
smeredith0044 <sydney.meredith@ymail.com>
2022-11-05 20:41:32 -0400

    deserialized villager data and printed to console if theres a match

--- VillagerSearch/Program.cs
+++ VillagerSearch/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using VillagerSearch;
 namespace HttpClientStatus;
 
@@ -10,20 +12,48 @@ class Program
     {
 
         // Type your username and press enter
-       // Console.WriteLine("Enter villager name:");
+        Console.WriteLine("Enter villager name:");
 
         // Create a string variable and get user input from the keyboard and store it in the variable
-       // string villagerName = Console.ReadLine();
+        string villagerName = Console.ReadLine();
 
         // Print the value of the variable (userName), which will display the input value
-       // Console.WriteLine("villager is: " + villagerName);
+        Console.WriteLine("villager is: " + villagerName);
 
         using var client = new HttpClient();
 
         var result = await client.GetAsync("https://acnhapi.com/v1/villagers/");
         Console.WriteLine(result.StatusCode);
         string responseBody = await result.Content.ReadAsStringAsync();
-        JsonFileUtils.SimpleWrite(responseBody, "/Users/sydney/Sydney_final_project/VillagerSearch/VillagerSearch/VillagerData.json");
-    }
+        dynamic objects = JsonConvert.DeserializeObject(@responseBody); // parse as array  
+        List<Villager> villagerList = new List<Villager>();
+        foreach (var root in objects)
+        {
+            try
+            {
+                string value = Convert.ToString(root.Value);
+                Villager villager = JsonConvert.DeserializeObject<Villager>(value);
+                villagerList.Add(villager);
+            } catch (SystemException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+            foreach (Villager villager in villagerList) {
+                foreach (KeyValuePair<string, string> name in villager.name) {
+
+                if (villagerName != null)
+                {
+                    if (name.Value.ToLower().Equals(villagerName.ToLower()))
+                    {
+                        Console.WriteLine("theres a match!");
+                        Console.WriteLine(villager.ToString());
+
+                        break;
+                    }
+                }
+                }
+            }
+        }
 }
 
---
+++ VillagerSearch/Villager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+
+namespace VillagerSearch
+{
+    public class Villager
+    {
+        public string id { get; set; }
+        public IDictionary<string, string> name { get; set; }
+        public string personality { get; set; }
+        public string birthday { get; set; }
+        public string species { get; set; }
+        public string gender { get; set; }
+        public string subtype { get; set; }
+        public string hobby { get; set; }
+        //public string catch-phrase { get; set; }
+        public string icon_uri { get; set; }
+        public string image_uri { get; set; }
+        //public string bubble-color { get; set; }
+        //public string text-color { get; set; }
+        public string saying { get; set; }
+        //public IDictionary<string, string> catch-translations { get; set; }
+
+        // extra fields
+        [Newtonsoft.Json.JsonExtensionData]
+        private IDictionary<string, JToken> _extraStuff;
+        public Villager()
+        {
+        }
+
+        override
+        public string ToString()
+        {
+            var jsonString = JsonConvert.SerializeObject(
+           this, Formatting.Indented,
+           new JsonConverter[] { new StringEnumConverter() });
+            return jsonString;
+        }
+    }
+}
+
